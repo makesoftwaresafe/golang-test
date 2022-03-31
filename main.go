c3c71398986414d7943dfc32e9acc7f980a91b95
rizaziz2002 <rizaziz2002@forallsecure.com>
2022-03-31 10:16:46 -0400

    This is starting point

--- main.go
+++ main.go
@@ -28,6 +28,22 @@ func main() {
 	file.Read(buffer)
 
 	if len(buffer) >= 3 {
+		if buffer[0] == 'b' {
+			if buffer[1] == 'u' {
+				if buffer[2] == 'g' {
+					panic("This is a bug 1")
+				}
+			}
+		}
+
+		if buffer[0] == 'b' {
+			if buffer[1] == 'g' {
+				if buffer[2] == 'u' {
+					panic("This is a bug 2")
+				}
+			}
+		}
+
 		if buffer[0] == 'u' {
 			if buffer[1] == 'b' {
 				if buffer[2] == 'g' {
